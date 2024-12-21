d35d6163b22ad5826870a9529e822dbe8d7a5165
debor <debor@DEBORAH>
2024-12-21 18:49:13 +0100

    Add project files.

---
+++ BlackNuse/Form1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace BlackNuse
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+      
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ForlornApi.Api.Inject();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ForlornApi.Api.ExecuteScript(richTextBox1.Text);
+        }
+
+        private void guna2PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+    }
+
